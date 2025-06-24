5e53b6e3395d6b4e1bdcac2d04c8a7ebcb3b66aa
salom <salom@10.247.176.152>
2025-06-24 10:26:07 -0500

    Initial commit

---
+++ backendDesarrollo1/src/main/java/com/desarrollo/backend/BackendDesarrollo1Application.java
@@ -0,0 +1,13 @@
+package com.desarrollo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesarrollo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesarrollo1Application.class, args);
+	}
+
+}
---
+++ backendDesarrollo1/src/test/java/com/desarrollo/backend/BackendDesarrollo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesarrollo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
