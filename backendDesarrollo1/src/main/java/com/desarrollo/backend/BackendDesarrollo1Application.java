a6da4e70c55bcc423f4547e8311f6194651a943c
salom <salom@DESKTOP-EBT6A40>
2025-06-28 17:13:17 -0500

    Avance 1

--- backendDesarrollo1/src/main/java/com/desarrollo/backend/BackendDesarrollo1Application.java
+++ backendDesarrollo1/src/main/java/com/desarrollo/backend/BackendDesarrollo1Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendDesarrollo1Application {
+public class BackendDesarrollo1Application{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendDesarrollo1Application.class, args);
 	}
+
 
 }
